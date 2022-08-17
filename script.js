24f82e6966f01ac85bd2db0ab6e7fadd94b4039a
AghasiStepanyan <stepanyan.aghasi@bk.ru>
2022-08-17 21:04:31 +0400

    first add files

---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+// primitive 
+
+// number
+
+
+
+
+
+
+let x = 15;
+let y = 15.2;
+
+
+
+// +- infinity NaN
+
+
+// string
+
+let surname = "Yeghiazaryan" 
+let age = 35 
+// let str = `my name is Anushavan ${age}`
+console.log(age);
+// console.log('my age is', + age, + "",  str)
+// console.log(str);
+// let mySurnameValue = "Yeghiazaryan"
+// let my_surname_value = "Yeghiazaryan"
+// const _ = "value"
+// const $ = "value"
+
+// console.log(mySurnameValue);
+// console.log(_);
+
+// // boolean
+// // true false
+
+
+// // let conf = confirm("Grancvac eq te voch")
+// let p = prompt("tye your name")
+// console.log(p);
+// console.log(conf);
+
+
+// null
+
+let u;
+
+
+console.log(u);
+
+u = "name"
+console.log(u);
+
+let n = 15;
+console.log(n);
+
+n = null
+console.log(n);
+
+// undefined
+// no primitive
